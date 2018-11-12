47746c99e0f9cbf4938d97fa26e3b3e0acd4d750
Unknown <m.bachorski@gmail.com>
2018-11-12 23:23:13 +0100

    Adds simple header

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 /** @format */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header />
+);
 
 AppRegistry.registerComponent(appName, () => App);
