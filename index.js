1e0f3dd9cfdb1b1100c605c91b2764c654b85146
Unknown <m.bachorski@gmail.com>
2018-11-12 23:59:32 +0100

    Header styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {name as appName} from './app.json';
 import Header from './src/components/header';
 
 const App = () => (
-    <Header />
+    <Header headerText={'Albums'}/>
 );
 
 AppRegistry.registerComponent(appName, () => App);
