1c986018983b13a73d65460d6983c10682ac1595
Unknown <m.bachorski@gmail.com>
2018-11-13 23:54:42 +0100

    Adds simple http call for albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+  // state initialization
+  state = { albums: [] };
+
+  componentWillMount(){
+    // console.log('componentWillMount in AlbumList');
+    // debugger;
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    // .then(response => console.log(response));
+    // setState is Component's method
+    .then(response => this.setState({ albums: response.data }));
+  }
+
   render() {
+    console.log(this.state)
+
     return (
       <View>
         <Text>Album List!</Text>
