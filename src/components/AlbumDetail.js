14b33a350fa7fdcc2660b9ce837d3deea6d511c5
Marek Bachorski <m.bachorski@gmail.com>
2018-11-21 08:32:03 +0100

    ADDS flow and linter to the project

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,16 @@
+// @flow
+
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail'
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
   // state initialization
   state = { albums: [] };
 
-  componentWillMount(){
+  componentWillMount() {
     // console.log('componentWillMount in AlbumList');
     // debugger;
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
@@ -20,12 +22,12 @@ class AlbumList extends Component {
   renderAlbums() {
     return this.state.albums.map(album =>
       // it should be id, not title
-      <AlbumDetail key={album.title}  album={album} />
+      <AlbumDetail key={album.title} album={album} />
     );
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
 
     return (
       <View>
